669d71696e5c2b01bdea03d6499ff93f6118c232
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-11 17:56:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $show = substr($arrJson['events'][0]['message']['text'], 0, 1);
 $idcard = substr($arrJson['events'][0]['message']['text'], 1);
 if ($show == "#") {
     if ($idcard != "") {
-        $urlWithoutProtocol = "vpn.idms.pw/id_pdc/run_pdc.php?" . $idcard;
+        $urlWithoutProtocol = "http://vpn.idms.pw/id_pdc/run_pdc.php?" . $idcard;
         $isRequestHeader = FALSE;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
