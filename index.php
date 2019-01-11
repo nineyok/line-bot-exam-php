e947149508d9193a714e4dca9534077e09af6a19
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-11 18:46:48 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if ($show == "#") {
         $arrPostData = array();
                 $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
         $arrPostData['messages'][0]['type'] = "text";
-        $arrPostData['messages'][0]['text'] = "เบอร์โทร : ". $idcard . "\r\n"
+        $arrPostData['messages'][0]['text'] = "เลขที่บัตร : ". $idcard . "\r\n"
 		        . "จำนวน : " . $Real_Service_Amount . "  บาท" ."\r\n"
                 /* . "เครือข่าย : " . $Service_Type . "\r\n"
 				. "เติมล่าสุด : " . $Start_date . "\r\n"
