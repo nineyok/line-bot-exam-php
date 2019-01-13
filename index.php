b43834f55a9ef9d8a4bd7c6036e85953764f7f6a
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-13 21:18:56 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -143,11 +143,9 @@ if($strchk[0]=="#"){
                   $numLine = count(explode("\n",$productivity));
 				  
                   for ($start=0; $start < count($Real_Service); $start++) {
-                  if($Real_Service[$start]!="" or ($start < ($numLine-3))){
+                  if($Real_Service[$start]!=""){
 	                  $Real_Service_Amount = $Real_Service_Amount.$Real_Service[$start] "\r\n";
-                  }elseif($start == ($numLine-3)){
-					  $Real_Service_Amount = $Real_Service_Amount.$Real_Service[$start].$start;
-				  }     
+                  }   
                   }  
 				  
 				     //$Real_Service_Amount = $productivity;
