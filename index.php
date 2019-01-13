fef1a89101b667ff4f9841da9e71dde5d5a58cac
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-13 20:28:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -320,7 +320,7 @@ if($strchk[0]=="#"){
 				  
                   for ($start=0; $start < count($Real_Service); $start++) {
                   if($Real_Service[$start]!=""){
-	              $Real_Service_Amount = $start."1".$Real_Service[$start] . "<BR>";
+	              $Real_Service_Amount = $Real_Service_Amount.$Real_Service[$start] . "<BR>";
                   }     
                   }  
 				  
