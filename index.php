69aae2169f00a6d726104ccf584c795154f37fec
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-14 21:20:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -424,7 +424,7 @@ if($strchk[0]=="#"){
 		
 		$Real_Service_Amount = $arrbn_id[0];  //จำนวนเงิน
         $Service_Type = $arrbn_id[1]; //เครือข่าย
-		$id = $arrayJson['events'][0]['source']['userId'];
+		$id = $arrJson['events'][0]['source']['userId'];
 		$txt = "ธนาคาร : ". $arrbn_id[0] . "\r\n"
 		."สาขา : ".$arrbn_id[1].$id;
 		  if($arrbn_id[1]!=""){
