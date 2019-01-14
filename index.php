5d9bbea2e3ea86576200381e5216e9e3d84d4d9f
nineyok <38936997+nineyok@users.noreply.github.com>
2019-01-14 19:12:29 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -396,7 +396,7 @@ if($strchk[0]=="#"){
      //echo $text_output[0];
      //echo $text_output[1]; 
 	 $request = urlencode($text_output[0]);
-        $urlWithoutProtocol = "http://vpn.idms.pw/id_pdc/select_bank.php?uid=".$request."&aid=".$text_output[1];
+        $urlWithoutProtocol = "http://vpn.idms.pw/id_pdc/select_bank.php?uid=".$text_output[0]."&aid=".$text_output[1];
         $isRequestHeader = FALSE;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
@@ -410,7 +410,8 @@ if($strchk[0]=="#"){
         $Service_Type = $arrbn_id[1]; //เครือข่าย
 		
 		$txt = "ธนาคาร : ". $arrbn_id[0] . "\r\n"
-		."สาขา : ".$arrbn_id[1].$urlWithoutProtocol.$text_output[0].$text_output[1];
+		."สาขา : ".$arrbn_id[1].$urlWithoutProtocol. "\r\n"
+		.$text_output[0].$text_output[1];
 		  if($Real_Service_Amount!=""){
                       $arrPostData = array();
                       $arrPostData["idcard"] = $idcard;
